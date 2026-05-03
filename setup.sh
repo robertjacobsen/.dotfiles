4a5b839eb7f467d3eb23e16d2b17f8caf34065d7
Robert Jacobsen <robert@robertjacobsen.no>
2026-05-03 15:32:47 +0200

    Quote variables and move backup file to repo

--- setup.sh
+++ setup.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 DOTFILES=$(cd "$(dirname "$0")" && pwd)
+BACKUP_DIR="$DOTFILES/.bak"
+
 ARCH=$(uname)
 BASHRC=".bashrc"
 if [[ "$ARCH" = "Darwin" ]]; then
     BASHRC=".bash_profile"
 fi
 
-cd ~
-if [ ! -d .bak ]; then
+if [ ! -d "$BACKUP_DIR" ]; then
     echo ".bak dir not found. Creating it...";
-    mkdir .bak
+    mkdir "$BACKUP_DIR"
 fi
 
 echo "Moving existing files to backup directory..."
-mv $BASHRC .bak
-mv .gitconfig .bak
-mv .vim .bak
-mv .vimrc .bak
+mv "$BASHRC" "$BACKUP_DIR"
+mv "$HOME/.gitconfig" "$BACKUP_DIR"
+mv "$HOME/.vim" "$BACKUP_DIR"
+mv "$HOME/.vimrc" "$BACKUP_DIR"
 
 echo "Linking config files..."
-ln -sf $DOTFILES/bashrc $HOME/$BASHRC
-ln -sf $DOTFILES/p10k.zsh $HOME/.p10k.zsh
-ln -sf $DOTFILES/zshrc $HOME/.zshrc
-ln -sf $DOTFILES/gitconfig $HOME/.gitconfig
-ln -sf $DOTFILES/vim $HOME/.vim
-ln -sf $DOTFILES/vimrc $HOME/.vimrc
-ln -sf $DOTFILES/ghostty $HOME/.config/ghostty
+ln -sf "$DOTFILES/bashrc" "$HOME/$BASHRC"
+ln -sf "$DOTFILES/p10k.zsh" "$HOME/.p10k.zsh"
+ln -sf "$DOTFILES/zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES/gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES/vim" "$HOME/.vim"
+ln -sf "$DOTFILES/vimrc" "$HOME/.vimrc"
+ln -sf "$DOTFILES/ghostty" "$HOME/.config/ghostty"
 
 echo "Done. Please relog into your shell."
